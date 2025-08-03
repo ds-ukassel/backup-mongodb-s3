37f890d9fb4640d98552656866cb663a8d7ef877
Adrian Kunz <a.kunz@uni-kassel.de>
2025-08-03 10:46:37 +0200

    fix: Docker image
    - Use alpine as base
    - Install mongodb-tools and minio-client via apk
    - Fix ENV syntax
    - Add docker-compose

--- scripts/backup-mongodb.sh
+++ scripts/backup-mongodb.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 SCRIPT_NAME=backup-mongodb
 ARCHIVE_NAME=mongodump_$(date +%Y%m%d_%H%M%S).gz
 OPLOG_FLAG=""
+MC_CMD="${MC_CMD:-mc}"
 MC_CONF_DIR="/scripts"
 
 export MC_HOST_backup=$S3_URI
@@ -13,7 +14,8 @@ if [ -n "$MONGODB_OPLOG" ]; then
         OPLOG_FLAG="--oplog"
 fi
 
-mc -C $MC_CONF_DIR mb -p backup/${S3_BUCKET} --insecure
+echo "[$SCRIPT_NAME] Creating Minio bucket if it does not exist: $S3_BUCKET"
+$MC_CMD -C $MC_CONF_DIR mb -p backup/${S3_BUCKET} --insecure
 
 echo "[$SCRIPT_NAME] Dumping all MongoDB databases to compressed archive and than sending to Minio object storage"
 
@@ -22,8 +24,8 @@ mongodump $OPLOG_FLAG \
         --gzip \
         --readPreference=$MONGODB_READ_PREFERENCE \
         --uri "$MONGODB_URI" | \
-mc -C $MC_CONF_DIR pipe "backup/$S3_BUCKET/$ARCHIVE_NAME" --insecure
+$MC_CMD -C $MC_CONF_DIR pipe "backup/$S3_BUCKET/$ARCHIVE_NAME" --insecure
 echo "[$SCRIPT_NAME] Succsesfull uploading $ARCHIVE_NAME to $S3_BUCKET bucket"
 
 echo "[$SCRIPT_NAME] Removing backups older than $RETENTION_PERIOD"
-mc -C $MC_CONF_DIR rm --recursive --older-than=${RETENTION_PERIOD} --force "backup/$S3_BUCKET" --insecure
+$MC_CMD -C $MC_CONF_DIR rm --recursive --older-than=${RETENTION_PERIOD} --force "backup/$S3_BUCKET" --insecure
